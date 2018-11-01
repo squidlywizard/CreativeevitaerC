db7383ceb3c2e51c3b49f2324a716e0723fa3185
squidlywizard <ztdenna@hotmail.com>
2018-11-01 09:41:21 -0600

    made stickmans head red

--- CreativeevitaerC/view/DrawingView.swift
+++ CreativeevitaerC/view/DrawingView.swift
@@ -17,13 +17,49 @@ public class DrawingView: UIView
     public override func draw(_ rect: CGRect) -> Void
     {
         // Drawing code
+        createStickFigure().stroke()
+        drawTurtle()
+        somethingElse()
     }
  
     private func createStickFigure() -> UIBezierPath
     {
         let figure : UIBezierPath = UIBezierPath()
         
-        return figure;
+        UIColor.purple.setStroke()
+        figure.lineWidth = 3.0
+        
+        figure.addArc(withCenter: CGPoint(x: 200, y: 200), radius: CGFloat(20), startAngle: CGFloat(0), endAngle: CGFloat(2) * CGFloat.pi, clockwise: true)
+        
+        figure.move(to: CGPoint(x: 200, y: 220))
+        figure.addLine(to: CGPoint(x: 200, y: 270))
+        figure.move(to: CGPoint(x: 180, y: 240))
+        figure.addLine(to: CGPoint(x: 220, y: 240))
+        figure.move(to: CGPoint(x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 180, y: 300))
+        figure.move(to: CGPoint(x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 220, y: 300))
+        return figure
+    }
+    private func drawTurtle() -> Void
+    {
+        let logo = UIBezierPath()
+        UIColor.white.setFill()
+        logo.move(to: CGPoint(x: 50, y: 250))
+        logo.addLine(to: CGPoint(x: 100, y: 300))
+        logo.addLine(to: CGPoint(x: 50, y: 350))
+        logo.close()
+        logo.fill()
+        
+    }
+    private func somethingElse() ->Void
+    {
+    let somting = UIBezierPath()
+        UIColor.red.setFill()
+        somting.addArc(withCenter: CGPoint(x: 200, y: 200), radius: CGFloat(20), startAngle: CGFloat(0), endAngle: CGFloat(2) * CGFloat.pi, clockwise: true)
+        somting.move(to: CGPoint(x: 200, y: 220))
+        somting.close()
+        somting.fill()
     }
 
 }
