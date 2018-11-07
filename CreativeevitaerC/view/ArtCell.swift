d07cc7fa496a4879e0813ad6c1fbfd82feaebeb3
squidlywizard <ztdenna@hotmail.com>
2018-11-07 09:40:15 -0700

    made art cell

---
+++ CreativeevitaerC/view/ArtCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArtCell.swift
+//  CreativeevitaerC
+//
+//  Created by Denna, Zachary on 11/7/18.
+//  Copyright © 2018 Denna, Zachary. All rights reserved.
+//
+
+import UIKit
+
+
+
+public class ArtCell: UICollectionViewCell
+{
+
+}
